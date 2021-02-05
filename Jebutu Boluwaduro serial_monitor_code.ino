b7d6a92919c3d5d470311fe4d24c330e8470ba9b
bolujebutu <78595576+bolujebutu@users.noreply.github.com>
2021-02-05 12:09:57 +0100

    Add files via upload

---
+++ Jebutu Boluwaduro serial_monitor_code.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello My name is JEBUTU BOLUWADURO, 16CJ020733");
+  delay(3000);
+}
